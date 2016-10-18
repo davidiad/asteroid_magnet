82606ad8ed3cd414fbeff23d1abc434b3ddc9e46
David Fierstein <studio@davidiad.com>
2016-10-18 16:49:43 -0700

    Create/import improved asteroid, and an asteroid disposal space station

---
+++ Assets/Scripts/Disposal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disposal : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+	}
+}
