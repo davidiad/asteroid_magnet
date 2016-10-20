d73bc601a101dc3650f66bf7f181826a9e3db4a8
David Fierstein <studio@davidiad.com>
2016-10-19 20:39:26 -0700

    Add particle sprite particle effects to improved asteroid
    Sprites that look like bits of rock coming off the tail of the asteroid. Add a trail renderer effect to the asteroid. Add a force and torque to the asteroid's rigidbody so that it interacts with gravity.

---
+++ Assets/Scripts/AsteroidB.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidB : MonoBehaviour {
+
+	public float thrust;
+	public ParticleSystem particles;
+	private Rigidbody rb;
+
+	void Awake() 
+	{
+		//Vector3 dir = GetComponent<ParticleSystem> ().transform.forward;
+		rb = GetComponent<Rigidbody>();
+		rb.AddForce(particles.transform.forward * thrust * -1f);
+
+	}
+
+	void FixedUpdate() 
+	{
+		//transform.LookAt (rb.velocity);
+		//rb.AddForce(particles.transform.forward * thrust * -0.01f);
+		rb.AddTorque(particles.transform.forward * 0.01f);
+	}
+}
